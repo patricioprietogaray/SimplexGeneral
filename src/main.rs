50541e0f8ded648086b89e64d38a43b554b8c503
patricioprietogaray <patricioprietogaray@gmail.com>
2022-03-25 20:31:16 -0300

    main

--- src/main.rs
+++ src/main.rs
@@ -22,21 +22,85 @@ fn main() {
 
     println!("Total de columnas es {}", _total_columnas);
 
+    println!("");
+    linea_de_la_tabla(_total_columnas);
+
+    // for i in 1..=((_total_columnas+1)*10) {
+    //     print!("_");
+    // }
+    // println!("");
+
+    // columna titulos encolumnados (eje y)
+    print!("* {0: <7} *", "");
+    print!(" {0: <7}  *", "Z");
+
+    let _letra ='x';
+    variables_en_el_titulo(_cant_productos, &_letra);
+
+    let _letra='s';
+    variables_en_el_titulo(_cant_var_holgura, &_letra);
+
+    println!(" {0: <7}  *", "R");
+
+    linea_de_la_tabla(_total_columnas);
+    
+    // for i in 1..=_cant_productos {
+    //     //concatenar dos cosas
+    //     let _i_string=i.to_string();
+    //     let _equis = "x";
+    //     let _concatenado= format!("x{}", _i_string);
+    //     print!(" {0: <7}  |", _concatenado);
+    // }
+
+    // for h in 1..=_cant_var_holgura {
+    //     // concatenar
+    //     let _s_string=h.to_string();
+    //     let _hache="h";
+    //     let _concatenar_holgura=format!("s{}", _s_string);
+    //     print!(" {0: <7} |", _concatenar_holgura);
+    // }
+
+
+
+
+    // println!("| {0: <7} | {1: <7} | {2: <7} | {3: <7} | {4: <7} | {5: <7} | {6: <7} |",
+    // " ","Z","x1","x2", "S1", "S2", "R");
+
+
     //creo un vector vacio
-    let mut _vector_columnas = Vec::new();
+    //let mut _vector_columnas:Vec<f64> = Vec::new();
 
     //asigno los espacios inicializadon en 0 (0.0 porque es f64) ojo acepta solo usize
     // vectores anidados
-    _vector_columnas=vec![vec![0.0;_total_columnas as usize];_cant_restricciones as usize];
+    //_vector_columnas=vec![vec![0.0;_total_columnas as usize];_cant_restricciones as usize];
 
-    //cargo cada elemento con un 8 
-    for (i, fila) in _vector_columnas.iter().enumerate() {
-        for(j, columna) in fila.iter().enumerate() {
-            //print!("[f: {}][c: {}]= {}", i, j, columna);
-            print!("{columna: <10} ");
-        }
-        println!("");
-    }
+    //cargo cada linea con un salto de linea.
+    // for (i, fila) in _vector_columnas.iter().enumerate() {
+    //     for(j, columna) in fila.iter().enumerate() {
+            
+
+    //         let mut _cantidad = String::new();
+    //         io::stdin().read_line(&mut _cantidad).ok().expect("Error al ingresar los datos por el teclado!");
+    //         let _cantidad = f64::from_str(&_cantidad.trim()).unwrap();
+    //         // NO FUNCA ASI _vector_columnas=vec![fila][columna]=_cantidad;
+
+    //         _vector_columnas.push(_cantidad as vec![fila][columna]);
+    //         //print!("[f: {}][c: {}]= {}", i, j, columna);
+    //         //print!("{columna: <10} ");
+    //     }
+    //     println!("");
+    // }
+
+
+
+    //muestro cada linea con un salto de linea.
+    // for (i, fila) in _vector_columnas.iter().enumerate() {
+    //     for(j, columna) in fila.iter().enumerate() {
+    //         //print!("[f: {}][c: {}]= {}", i, j, columna);
+    //         print!("{columna: <10} ");
+    //     }
+    //     println!("");
+    // }
     
         
     //muestro cada linea con un salto de linea.
@@ -63,6 +127,50 @@ fn main() {
 
 
 
+    // prueba con vectores (almacenar el mismo tipo de datos)
+
+    // los vectores que se inicializan en cero (Vec::new) 
+    // deben ser muteables para agregar datos posteriormente
+    // let mut v: Vec<f32> = Vec::new();
+
+    // agregar elementos a la ultima posicion del vector
+    // v.push(1.0);
+    // v.push(2.0);
+    // v.push(3.0);
+    // println!("La longitud del vector es {}", v.len());
+    // println!("Eliminar el ultimo elemento ingresado al vector con pop");
+    // v.pop();
+    // println!("La longitud del vector es {}", v.len());
+
+    //                      0   1   2   3   4 
+    // let ve = vec![10, 12, 23, 34, 45];
+    // let _cero:&i32=&ve[0];
+    // let _uno:&i32=&ve[1];
+    // let _dos:&i32=&ve[2];
+    // let _tres:&i32=&ve[3];
+    // let _cuatro:&i32=&ve[4];
+    // println!("El tercer elemento de ve es: {}", _tres);
+
+    // println!("Muestra la posición de ve.get(1) es {:?}", ve.get(1));
+// Muestra la posición de ve.get(1) es Some(12)
+
+
+
+    // match ve.get(0) {  // busca por el indice
+    //     Some(0) => println!("El valor 10 es el elemento 0. Some(10)"),
+        // Some(12) => println!("El valor 12 es el elemento 1. Some(12)"),
+        // Some(23) => println!("El valor 23 es el elemento 2. Some(23)"),
+        // Some(34) => println!("El valor 23 es el elemento 3. Some(34)"),
+        // Some(45) => println!("El valor 23 es el elemento 4. Some(45)"),
+        // None => println!("None"),
+    // }
+
+    // if ve.get(4) {
+    //     println!("El cuarto elemento de ve");
+    // }
+
+
+
     
 
 }
@@ -73,4 +181,24 @@ fn ingreso_por_teclado(_mensaje: &str) -> isize {
     io::stdin().read_line(&mut _cantidad).ok().expect("Error al ingresar los datos por el teclado!");
     let _cantidad=isize::from_str(&_cantidad.trim()).unwrap();
     _cantidad
+}
+
+fn variables_en_el_titulo(_cant_variables:isize, _x_o_s: &char) {
+    
+    for a in 1..=_cant_variables {
+        // concatenar
+        let _cardinal=a.to_string();
+        let _letra=_x_o_s;
+        let _concatenar=format!("{}{}", _letra, _cardinal);
+        print!(" {0: <7} *", _concatenar);
+    }
+
+}
+
+fn linea_de_la_tabla(_total_columnas:isize) {
+    for i in 1..=((_total_columnas+1)*10) {
+        print!("*");
+    }
+    println!("");
+
 }
